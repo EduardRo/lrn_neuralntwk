6d5483fce30ada3a772b89f4387662074b7410f6
EduardRo <eduardstancu@yahoo.com>
2023-02-14 12:03:27 +0200

    p4 frst

---
+++ p4.py
@@ -0,0 +1,13 @@
+import sys
+import numpy as np
+import matplotlib
+#de mod can ajngla cpt respctv
+
+inputs = [1.0, 2.0, 3.0, 2.5] 
+
+weights =[[0.2, 0.8, -0.5, 1.0],[0.5, -0.91, 0.26, -0.5],[-0.26, -0.27, 0.17, 0.87]]
+
+biases=[2,3,0.5]
+
+output = np.dot(weights, inputs)+biases
+print(output)
